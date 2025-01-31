1ba383687113418df7863355e57b1f211c841350
Eity2021 <Eity2021akter@gmail.com>
2025-02-01 03:07:11 +0600

    add API

--- src/App.js
+++ src/App.js
@@ -20,20 +20,20 @@ import ContactMe from "./components/ContactMe/ContactMe";
 
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+  // const [load, upadateLoad] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     upadateLoad(false);
+  //   }, 1200);
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
     <Router>
-      <Preloader load={load} />
-      <div className="App" id={load ? "no-scroll" : "scroll"}>
+      {/* <Preloader load={load} /> */}
+      <div className="App">
         <Navbar />
         <ScrollToTop />
         <Routes>
--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,52 +1,73 @@
-import React from "react";
+import React , {useState}from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
-import axios from 'axios';
+// import { FaGithub } from "react-icons/fa";
+import { MdMore } from "react-icons/md";
+import ProjectsDetailsModal from "./ProjectsDetailsModal";
 
 
-function ProjectCards(props) {
-
-
-axios.get('http://localhost:9000/project')
-.then((response) => {
-console.log( ' response' , response.data )
-})
-.catch((error) => {
-// Code for handling the error
-})
-
-
+function ProjectCards(project) {
+const [modalShow, setModalShow] = useState(false);
+const {name, image ,description, liveLink ,gitLink, id} = project.project;
+// console.log('project', project.name);
+const navigateId = (id) => {
+  setModalShow(id)
+  setModalShow(true)
+}
+const descriptionPreview = description
+? description.split(" ").slice(0, 30).join(" ") + (description.split(" ").length > 30 ? "..." : "")
+: "";
 
   return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
+    <div>
+    <Card className="project-card-view" >
+        <Card.Img variant="top" src={image} alt="card-img"  style={{ borderRadius: "100px" }}/>
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Card.Text style={{ textAlign: "justify" }}>
+              {descriptionPreview}
+          </Card.Text>
+          {
+            gitLink && (
+              <Button variant="primary" href={gitLink} target="_blank">
+              <BsGithub /> &nbsp;
+               GitHub
+            </Button>
+            )
+          }
+          {
+            liveLink && (
+              <Button
+              variant="primary"
+              href={liveLink}
+              target="_blank"
+              style={{ marginLeft: "10px" }}
+            >
+              <CgWebsite /> &nbsp;
+              Live
+            </Button>
+            )
+          }
+            <Button
+              variant="primary"
+              style={{ marginLeft: "10px" }}
+              onClick={() => navigateId(id)}
+            >
+             <MdMore /> &nbsp;
+                More.. 
+            </Button>
+         
+        </Card.Body>
+      </Card>
+      <ProjectsDetailsModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        id={id}
+      />
+    </div>
 
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-        )}
-      </Card.Body>
-    </Card>
   );
 }
 export default ProjectCards;
