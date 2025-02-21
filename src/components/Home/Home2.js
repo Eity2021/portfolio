83719a2dfc13a92d0bf9f52c442d23f42da07b76
Eity2021 <Eity2021akter@gmail.com>
2025-02-22 00:24:59 +0600

    changed

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -139,7 +139,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/marufa_akter_eity/"
+                  href="https://www.instagram.com/marufa_akter_eity/d"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
